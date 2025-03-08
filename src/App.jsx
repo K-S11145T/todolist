98f99191b8e7c4cb5ce0c80a7c35923293b2b42f
K-S11145T <tanushsingh843@gmail.com>
2025-03-08 14:42:26 +0530

    route handle completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import Login from "./pages/Login";
 import ToDo from "./pages/ToDo";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 text-gray-800">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/todo" element={<ProtectedRoute><ToDo /></ProtectedRoute>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
